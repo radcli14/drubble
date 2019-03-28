Fixed the nPlayer selection option screen, and background scaling.

drubbleFunc.py
@@ -260,7 +260,7 @@ def drawBackgroundImage(img,rect,xpos,ypos,m2p):
         image(bg0,left,bottom,rect[0],rect[1])
     
 def makeGameImage():
-    for k in range(nPlayer):
+    for k in range(p.nPlayer):
         # Get the plotting vectors using stickDude function
         xv,yv,sx,sy = stickDude(gs.u,k)
             
@@ -418,8 +418,10 @@ class parameters:
     zt = 0.05    # Stool tilt damping ratio
     Ct = 2*zl*np.sqrt(Kt*m) # Tilt damping [N-m-s/rad]
 
+    # Gameplay settings
     userControlled = np.array([[True, True, True, True ],
                                [False,False,False,False]]) 
+    nPlayer = 1
 
     # Stool parameters
     xs = np.array([-0.25,  0.25,  0.14, 
@@ -605,7 +607,7 @@ class gameState:
         self.n += 1
         
         # Active player
-        pAct = np.mod(stats.stoolCount,nPlayer)
+        pAct = np.mod(stats.stoolCount,p.nPlayer)
         
         # Initial assumption, there was no event
         self.StoolBounce = False
@@ -648,7 +650,7 @@ class gameState:
             # Change ball states depending on if it was a stool or floor bounce
             if self.StoolBounce:
                 # Obtain the bounce velocity
-                vBounce,vRecoil = BallBounce(self,np.mod(stats.stoolCount,nPlayer))
+                vBounce,vRecoil = BallBounce(self,np.mod(stats.stoolCount,p.nPlayer))
                 self.ue[2] = vBounce[0]
                 self.ue[3] = vBounce[1]
                 
@@ -789,8 +791,8 @@ def BallPredict(gs):
     # Solve for time that the ball would hit the ground
     tG = -(-gs.dyb-np.sqrt(gs.dyb**2+2.0*p.g*gs.yb))/p.g
 
-    # Solve for the arc for the next one second, or until ball hits ground
-    T     = np.linspace(0,min(1.0,tG),20)
+    # Solve for the arc for the next 1.2 seconds, or until ball hits ground
+    T     = np.linspace(0,min(1.2,tG),20)
     xTraj = gs.xb+gs.dxb*T
     yTraj = gs.yb+gs.dyb*T-0.5*p.g*T**2
     
@@ -816,7 +818,7 @@ def PlayerAndStool(t,u):
     du[0:4] = [dxb,dyb,0,-p.g] # Ball velocities and accelerations
     
     # Loop over players
-    for k in range(nPlayer):
+    for k in range(p.nPlayer):
         # Create player state vectors
         q  = np.matrix([[xp[k]],[yp[k]],[lp[k]],[tp[k]]])
         dq = np.matrix([[dxp[k]],[dyp[k]],[dlp[k]],[dtp[k]]])
@@ -956,7 +958,7 @@ def ControlLogic(t,u,k):
     # Control horizontal acceleration based on zero effort miss (ZEM)
     # Subtract 1 secoond to get there early, and subtract 0.01 m to keep the
     # ball moving forward 
-    ZEM = (gs.xI+10*(nPlayer-1-np.mod(stats.stoolCount,2))-0.1) - xp[k] - dxp[k]*np.abs(gs.timeUntilBounce-1)
+    ZEM = (gs.xI+10*(p.nPlayer-1-np.mod(stats.stoolCount,2))-0.1) - xp[k] - dxp[k]*np.abs(gs.timeUntilBounce-1)
     if p.userControlled[k,0]:
         if gs.ctrl[0] == 0:
             try:

main.py
@@ -27,7 +27,6 @@ p = parameters()
 q0 = np.matrix([[0],[p.y0],[p.l0],[0]])
 u0 = [0,p.rb,0,0,p.x0,p.y0,p.l0,0,0,0,0,10,
                  0   ,p.y0,p.l0,0,0,0,0,-10]
-nPlayer = 2
 gs = gameState(u0)
 
 # Set the keyboard input and mouse defaults
@@ -54,10 +53,11 @@ class MyBackground(Widget):
     def __init__(self, **kwargs):
         super(MyBackground, self).__init__(**kwargs)
         self.xpos = 0
+        self.sz_orig = self.w_orig,self.h_orig = (2400,400)
         with self.canvas:
             # Import the background image
             self.bg = Rectangle(source='figs/bg0.png', 
-                                pos=(0,0), size=(2400,400))
+                                pos=(0,0), size=self.sz_orig)
 
              # Draw the bottom line
             Color(black[0], black[1], black[2],1)
@@ -65,9 +65,9 @@ class MyBackground(Widget):
 
     def update(self, x, y, w, h, m2p): 
         scf = (m2p/70)**0.5
-        posInBG = x/60-self.xpos # Position in the Background
-        self.bg.size = (int(2400*scf),int(400*scf))
-        self.bg.pos = (w*(-posInBG)-(2400*posInBG-self.bg.size[0])/2.0,h/20)
+        posInBG = x/100*scf*(self.w_orig-self.xpos) # Position in the Background
+        self.bg.size = (int(self.w_orig*scf),int(self.h_orig*scf))
+        self.bg.pos = (w/2.0-posInBG,0)
         self.bl.size = (w,h/20)
 
 class splashScreen(Widget):
@@ -214,12 +214,12 @@ class drubbleGame(Widget):
     
     def on_touch_down(self, touch):
         if gs.gameMode==2 and touch.y>self.height*0.67:
-            nPlayer=1
-            print('nPlayer=',str(nPlayer))
+            p.nPlayer=1
+            print('nPlayer=',str(p.nPlayer))
             cycleModes(gs,stats)
         elif gs.gameMode==2 and touch.y>self.height*0.33:
-            nPlayer=2
-            print('nPlayer=',str(nPlayer))
+            p.nPlayer=2
+            print('p.nPlayer=',str(p.nPlayer))
             cycleModes(gs,stats)
         elif gs.gameMode>2:
             # Cycle through modes if touch above the halfway point
@@ -281,7 +281,7 @@ class drubbleGame(Widget):
                 Color(white[0], white[1], white[2], 1)
                 Line(points=p1.stool,width=0.05*p1.m2p)
                 
-                if nPlayer>1:
+                if p.nPlayer>1:
                     # Draw Player Two
                     Color(red[0], red[1], red[2], 1)
                     Line(points=p2.player,width=0.075*p2.m2p)
@@ -319,7 +319,7 @@ class drubbleGame(Widget):
         elif gs.gameMode>2 and not self.weHaveWidgets:
             self.add_game_widgets()            
             self.weHaveWidgets = True            
-            print('nPlayer=',str(nPlayer))   
+            print('nPlayer=',str(p.nPlayer))   
             
         ## ANGLE AND SPEED SETTINGS
         if gs.gameMode>2 and gs.gameMode<6: